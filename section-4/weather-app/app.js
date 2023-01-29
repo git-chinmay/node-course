d2e13df2f8300858ddfe08c44359548da1528895
CICADA3310 <36794918+git-chinmay@users.noreply.github.com>
2023-01-29 12:04:47 +0530

    Making a HTTP request

--- section-4/weather-app/app.js
+++ section-4/weather-app/app.js
@@ -1,16 +1,10 @@
-console.log("Starting...");
-setTimeout(()=>{
-    console.log("Timer-1...");
-},2000);
-
-setTimeout(()=>{
-    console.log("Timer-2...");
-},0);
-
-console.log("Stopping...");
-
-/* 
-Here expected sequence was Starting->Timer-2->Stopping->Timer-1
-Bcz 0ms in Timer-2 it should start before Stopping but nothing such happening here.
-To know why we have to understand the internals of Node.js(Call Stack, Call Back Queue & Event loop)
-*/
+const request = require('postman-request');
+const url = 'http://api.weatherstack.com/current?access_key=5cd06d83c6a1ec72ea659dc2dcdb69c9&query=paradeep'
+request(url, (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', body); // Print the raw JSON body
+  const data = JSON.parse(body);
+  //console.log(data); //print the body in JS object
+  console.log(data.current);
+});
