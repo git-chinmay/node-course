9ab588a37e4cb825aea65c8408ad10e52178b8d5
CICADA3310 <36794918+git-chinmay@users.noreply.github.com>
2023-01-31 11:33:40 +0530

    error handeling added.

--- section-4/weather-app/app.js
+++ section-4/weather-app/app.js
@@ -1,10 +1,19 @@
 const request = require('postman-request');
 const url = 'http://api.weatherstack.com/current?access_key=5cd06d83c6a1ec72ea659dc2dcdb69c9&query=paradeep'
-request(url, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the raw JSON body
-  const data = JSON.parse(body);
-  //console.log(data); //print the body in JS object
-  console.log(data.current);
+//const url = 'http://api.weatherstack.com/current?access_key=5cd06d83c6a1ec72ea659dc2dcdb69c9&query'
+request({url:url, json:true}, (error, response, body) => {
+
+    if(error){
+        console.log("Weather service not avilable");
+        console.log('error:', error); // Print the error if one occurred
+    }else if(body.error){
+        console.log("Please specify a valid location identifier using the query parameter.");
+    }else{
+
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        //const data = JSON.parse(body); As we are using the json=true
+        //console.log(body.current);
+        console.log(`${body.current.weather_descriptions[0]} : It is currenlty ${body.current.temperature} degree out but feels like ${body.current.feelslike} degree.`);
+
+    }  
 });
