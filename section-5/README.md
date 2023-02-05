806c101f9b2be0ff7579bf5e2c622739e0691885
CICADA3310 <36794918+git-chinmay@users.noreply.github.com>
2023-02-05 17:01:36 +0530

    serving static asset

---
+++ section-5/README.md
@@ -0,0 +1,2 @@
+### Running node script in a webserver (Express server)
+#### Express is a node based simple web server https://expressjs.com/
--- section-5/web-server/src/app.js
+++ section-5/web-server/src/app.js
@@ -1,28 +1,54 @@
 const express = require('express')
+const path = require('path') //core module no need to install
 
-const app = express();
-
+// NOTES FOR QUICK LEARNING //
 //req = Request received from user
 //res = what response server send back
+// console.log(__dirname); // gives the current directry
+// console.log(__filename); // gived the absolute path of the current script
+// console.log(path.join(__dirname, '../public'))
 
+
+const app = express();
+const pathToPublicDirectory = path.join(__dirname, '../public');
+
+//Making express to use the static public folder for rendering
+app.use(express.static(pathToPublicDirectory))
+
+//Bcz of above static express will never run below root page so we can comment/remove it
 //app.com
-app.get('', (req, res)=>{
-    res.send('Hello Express')
-})
+// app.get('', (req, res)=>{
+//     res.send('Hello Express')
+// })
+
+
+///Lets replace the /help and /about page with static html///
+//localhost:3000/about.html > give about page
+//localhost:3000/help.html > gives help page
+
 
 //app.com/help
-app.get('/help', (req, res)=>{
-    res.send('Help Page')
-})
+// app.get('/help', (req, res)=>{
+//     res.send({
+//         name: "Chinmay",
+//         location: "India"
+//     })
+// })
 
 //app/com/about
-app.get('/about', (req, res)=>{
-    res.send('About Page')
-})
+// app.get('/about', (req, res)=>{
+//     res.send('<h1>My Node js based app</h1>')
+// })
+
 
 //weather page
+//Passing JS object, express convert it inot json and returned to webpage
 app.get('/weather', (req, res)=>{
-    res.send('Weather Page')
+    res.send({
+        location: "paradeep",
+        temperture: 26,
+        unit: 'Centigrade'
+    })
 })
 
 //Starting the Express server
