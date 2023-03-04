23a2566eb2f024bc9c28eec58f33c62b01a265b1
CICADA3310 <36794918+git-chinmay@users.noreply.github.com>
2023-03-04 11:53:31 +0530

    task-manager playground updated to use async and await

--- section-8/task-manager/playground/promise-chaoning.js
+++ section-8/task-manager/playground/promise-chaoning.js
@@ -1,11 +1,26 @@
 const users = require("../src/models/users")
 require("../src/db/mongoose")
 
-users.User.findByIdAndUpdate('63fce78750913b77f800a430', {age:1}).then((user)=>{
-    console.log(user)
-    return users.User.countDocuments({age:1})
-}).then((result)=>{
-    console.log(result);
+// users.User.findByIdAndUpdate('63fce78750913b77f800a430', {age:1}).then((user)=>{
+//     console.log(user)
+//     return users.User.countDocuments({age:1})
+// }).then((result)=>{
+//     console.log(result);
+// }).catch((e)=>{
+//     console.log(e);
+// })
+
+
+
+// Rewriting above code using async and awit
+const updateAgeandCount = async (id, age) => {
+    const user = await users.User.findByIdAndUpdate(id, {age}); // age:age = age
+    const count = await users.User.countDocuments({age});
+    return count;
+}
+
+updateAgeandCount("63fe2ffc0bd46e73ac5c9073", 20).then((c)=>{
+    console.log("Count", c);
 }).catch((e)=>{
-    console.log(e);
+    console.log('Error', e);
 })
