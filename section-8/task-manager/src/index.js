57b68bd64e79baf5f417a6db11a2a4c77856f234
CICADA3310 <36794918+git-chinmay@users.noreply.github.com>
2023-03-04 12:37:27 +0530

    index.js updated with async and awaiy

--- section-8/task-manager/src/index.js
+++ section-8/task-manager/src/index.js
@@ -12,43 +12,86 @@ app.use(express.json());
 
 
 // Create an User
-app.post("/users", (req, res)=>{
-    console.log(req.body);
-    const user = new users.User(req.body);
-    user.save().then(()=>{
-        res.status(201).send(user);
-    }).catch((error)=>{
-        // res.status(400);
-        // res.send(error);
-        res.status(400).send(error);
-    })
+// app.post("/users", (req, res)=>{
+//     console.log(req.body);
+//     const user = new users.User(req.body);
+//     user.save().then(()=>{
+//         res.status(201).send(user);
+//     }).catch((error)=>{
+//         // res.status(400);
+//         // res.send(error);
+//         res.status(400).send(error);
+//     })
 
     
+// })
+
+
+// Rewriting above with async -await
+app.post("/users", async (req, res) => {
+    console.log(req.body);
+    const user = new users.User(req.body);
+    try{
+        await user.save()
+        res.status(201).send(user);
+    }catch (error) {
+        res.status(400).send(error);
+    }
 })
 
 
 //Get all user details
-app.get("/users", (req, res)=>{
-    users.User.find({}).then((users)=>{
-        res.send(users);
-    }).catch((e)=>{
+// app.get("/users", (req, res)=>{
+//     users.User.find({}).then((users)=>{
+//         res.send(users);
+//     }).catch((e)=>{
+//         res.status(500).send();
+//     })
+// })
+
+app.get("/users", async (req, res)=>{
+
+    try{
+        const usersx = await users.User.find({});
+        res.send(usersx);
+    }catch(e){
         res.status(500).send();
-    })
+    }
 })
 
 
 //Get an user by id
-app.get("/users/:id", (req, res)=>{
+// app.get("/users/:id", (req, res)=>{
+//     //console.log(req.params); //{id: '12345'}
+//     const _id = req.params.id;
+//     users.User.findById(_id).then((userx) => {
+//         if (!userx){
+//             return res.status(404).send();
+//         }
+//         res.send(userx);
+//     }).catch((e)=>{
+//         res.status(500).send("Something went wrong!");
+//     })
+// })
+
+
+app.get("/users/:id", async (req, res)=>{
     //console.log(req.params); //{id: '12345'}
     const _id = req.params.id;
-    users.User.findById(_id).then((userx) => {
-        if (!userx){
+    
+    try{
+
+        const user = await users.User.findById(_id)
+
+        if (!user){
             return res.status(404).send();
         }
-        res.send(userx);
-    }).catch((e)=>{
+        res.send(user);
+
+    }catch(e){
         res.status(500).send("Something went wrong!");
-    })
+        //res.status(500).send(e);
+    }
 })
 
 
