44d2312f75294d1c9607c852ae691a469ab80413
CICADA3310 <36794918+git-chinmay@users.noreply.github.com>
2023-03-10 11:24:06 +0530

    express middleware

--- section-9/README.md
+++ section-9/README.md
@@ -4,3 +4,6 @@
 - Hashing the password using bcryptjs
 - Login the user using email and password
 - Authenticate a successfully logged in user using Json Web Token to perform operations like delete and update.
+- Generate token while create or sign in 
+- Use token for any other task
+- Express Middleware
--- section-9/task-manager/src/index.js
+++ section-9/task-manager/src/index.js
@@ -7,11 +7,38 @@ const taskRouter = require('./routers/task')
 const app = express();
 const port = process.env.PORT || 3000
 
-//Parsing incoming data into object
-app.use(express.json());
+//Here we will use our custom defined function in our application
+//middleware function to block GET calls
+// app.use((req, res, next)=>{
+//     console.log(req.method, req.path);
+//     if (req.method === "GET"){
+//         res.send("GET requests are disabled on this API.")
+//     }else{
+//         next();
+//     }
+// })
+
+
+//CODE CHALLENGE: Block all API calls syaing Maintenance mode
+app.use((req, res, next)=>{
+    // if(req.method){
+    //     res.status(503).send("API is under maintenance. Please come back later!")
+    // }
+    // next();
+
+    //OR
+    res.status(503).send("API is under maintenance. Please come back later!")
+
+})
+
+// Here we are using the functions provided by express in our application
+app.use(express.json()); //Parsing incoming data into object
 app.use(userRouter);
 app.use(taskRouter);
 
+//WITHOUT MIDDLEWARE : new request --> run route handler
+
+//WITH MIDDLEWARE : new request --> do something(pre/post thing) --> run route handler
 
 app.listen(port, ()=>{
     console.log(`Server listining on port ${port}`);
