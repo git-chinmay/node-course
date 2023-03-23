b30d8e3e10869b244d4f5e1a9ed1136b5f493f81
CICADA3310 <36794918+git-chinmay@users.noreply.github.com>
2023-03-23 11:54:23 +0530

    jest setup and teardown

--- section-13/README.md
+++ section-13/README.md
@@ -2,4 +2,5 @@
 
 - Jest Testing framework
 - Writing tests and assertions
-- 
+- Express test server using supertest
+- Jest setup and teardown
--- section-13/task-manager/tests/user.test.js
+++ section-13/task-manager/tests/user.test.js
@@ -1,5 +1,20 @@
 const request = require('supertest');
 const app = require('../src/app');
+const user = require('../src/models/users')
+
+const userOne = {
+    name: "Mike",
+    email: "mike@email.com",
+    password: "testPass"
+}
+
+// We need to wipe out the existing data always before running the tests otherwise it will fail 
+// becaus ethese users were already tere due to previous test run.
+beforeEach(async ()=>{
+    await user.User.deleteMany();
+    await new user.User(userOne).save();
+})
+
 
 test("Should sign up a new user", async () => {
     await await request(app).post('/users').send({
@@ -8,3 +23,25 @@ test("Should sign up a new user", async () => {
         password: "myPass077"
     }).expect(201)
 })
+
+
+test("Should login existing user", async ()=>{
+    await request(app).post("/users/login").send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+
+test("Should not login non existing user", async ()=>{
+    await request(app).post("/users/login").send({
+        email: "random user",
+        password: "wrong password"
+    }).expect(400)
+})
+
+
+//For our case we dont need the aftereach
+// afterEach(()=>{
+//     console.log("After each..")
+// })
